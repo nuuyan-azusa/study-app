c232fb10f92148c2bb41aa064ca79396ab82dd4c
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-14 19:42:54 +0900

    コントローラーの設定

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,22 @@
 class OptionsController < ApplicationController
+
+  def new
+    @option = Option.new
+  end
+
+  def create
+    @option = Option.new(option_params)
+    if @option.valid?
+      @option.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def option_params
+    params.require(:option).permit(:name, :text, :url_text).merge(user_id: current_user.id)
+  end
+
 end
