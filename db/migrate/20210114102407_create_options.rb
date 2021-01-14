c0cbb19f292445c160b6f6853a2b72d56dd14e64
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-14 19:45:10 +0900

    カラムの修正

--- db/migrate/20210114102407_create_options.rb
+++ db/migrate/20210114102407_create_options.rb
@@ -1,7 +1,10 @@
 class CreateOptions < ActiveRecord::Migration[6.0]
   def change
     create_table :options do |t|
-
+      t.string :name,      null: false
+      t.text :text,        null: false
+      t.string :url_text
+      t.references :user,  foreign_key: true
       t.timestamps
     end
   end
