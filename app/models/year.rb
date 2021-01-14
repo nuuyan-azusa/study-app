6853b04b7d22ce518c57cffa0f18d1a9bd448f4d
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-14 16:06:01 +0900

    アソシエーション設定

---
+++ app/models/year.rb
@@ -0,0 +1,10 @@
+class Year < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '__' },
+    { id: 1, name: '2020' },
+    { id: 2, name: '2021' },
+  ]
+  include ActiveHash::Associations
+  has_many :diaries
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_091924) do
+ActiveRecord::Schema.define(version: 2021_01_14_065457) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_091924) do
     t.string "url_text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
