fe4b6a6958cc6908ecb56e330ae31f233287eafb
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-14 16:04:23 +0900

    activehashの導入

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,14 @@
 class Diary < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :day
+  belongs_to_active_hash :hour
+  belongs_to_active_hash :minute
+  belongs_to_active_hash :month
+  belongs_to_active_hash :year
+
+  with_options presence: true do
+    validates :title
+    validates :text
+  end
 end
