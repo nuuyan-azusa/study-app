4352ff9dfb3090f418e15c2a2c12ef4f876fc248
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-15 16:58:12 +0900

    READMEの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_102407) do
+ActiveRecord::Schema.define(version: 2021_01_15_072043) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 2021_01_14_102407) do
     t.index ["user_id"], name: "index_options_on_user_id"
   end
 
+  create_table "regular_expressions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.string "url_text"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_regular_expressions_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -61,4 +71,5 @@ ActiveRecord::Schema.define(version: 2021_01_14_102407) do
 
   add_foreign_key "manners", "users"
   add_foreign_key "options", "users"
+  add_foreign_key "regular_expressions", "users"
 end
