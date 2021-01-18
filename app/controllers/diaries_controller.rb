73b8b01f7af48a3a7ce761131f594e31efcece58
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-18 15:41:16 +0900

    Revert "コントローラーの設定"
    This reverts commit 303497baf2b235e33cdc08aa62632683df402023.

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,7 +1,4 @@
 class DiariesController < ApplicationController
-  before_action :set_diary, only: [:edit, :update]
-  before_action :move_diary, only: [:edit]
-
   def index
     @diaries = Diary.includes(:user).order('created_at DESC')
   end
@@ -11,7 +8,6 @@ class DiariesController < ApplicationController
   end
 
   def create
-    binding.pry
     @diary = Diary.new(diary_params)
     if @diary.valid?
       @diary.save
@@ -25,30 +21,9 @@ class DiariesController < ApplicationController
     @diary = Diary.find(params[:id])
   end
 
-  def edit
-  end
-
-  def update
-    if @diary.update(diary_params)
-      redirect_to diary_path(@diary.id)
-    else
-      render :edit
-    end
-  end
-
   private
   def diary_params
     params.require(:diary).permit(:title,:text,:year_id,:month_id,:day_id,:hour_id,:minute_id,:url_text).merge(user_id: current_user.id)
-  end
-
-  def set_diary
-    @diary = Diary.find(params[:id])
-  end
-
-  def move_diary
-    unless 
-      current_user.id == @diary.user.id  
-    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
-  resources :diaries, only: [:index, :new, :create, :show, :edit, :update]
+  resources :diaries, only: [:index, :new, :create, :show]
   resources :manners, only: [:index, :new, :create, :show]
   resources :options, only: [:index, :new, :create, :show]
   resources :expressions, only: [:index, :new, :create, :show]
