20ef32627c34d415054201ae9dd6a4fdeaf83145
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-18 16:32:38 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
   resources :diaries
-  resources :manners, only: [:index, :new, :create, :show, :edit, :update]
+  resources :manners
   resources :options, only: [:index, :new, :create, :show]
   resources :expressions, only: [:index, :new, :create, :show]
 end
