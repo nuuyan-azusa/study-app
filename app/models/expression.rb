05aaad862b413fffe9429137a3b3240018548206
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-19 15:31:52 +0900

    モデルの設定

--- app/models/expression.rb
+++ app/models/expression.rb
@@ -4,5 +4,13 @@ class Expression < ApplicationRecord
     validates :name
     validates :text
   end
+
+  def self.search(search)
+    if search != ""
+      Expression.where('name LIKE(?)', "%#{search}%")
+    else
+      Expression.all
+    end
+  end
   
 end
