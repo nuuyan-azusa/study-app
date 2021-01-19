83590420d12ec8a04e0b313ea12cab11f50f3ab1
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-19 15:20:12 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
-  resources :diaries
-  resources :manners
-  resources :options
-  resources :expressions
+  resources :diaries do
+    collection do
+      get 'search'
+    end
+  end
+  resources :manners do
+    collection do
+      get 'search'
+    end
+  end
+  resources :options do
+    collection do
+      get 'search'
+    end
+  end
+  resources :expressions do
+    collection do
+      get 'search'
+    end
+  end
 end
