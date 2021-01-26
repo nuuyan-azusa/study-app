3d529def2022ef98faec048ecb617e94fc25ee8d
nuuyan-azusa <nuuyan.azusa1105@gmail.com>
2021-01-26 18:08:25 +0900

    JavaScriptの設定

---
+++ app/javascript/menu.js
@@ -0,0 +1,20 @@
+function pullDown(){
+
+  const lists = document.getElementById("lists")
+  const menu = document.getElementById("menu")
+
+  menu.addEventListener('click', function(){
+    if (lists.getAttribute("style") == "display:block;"){
+      lists.removeAttribute("style", "display:block;")
+    } else {
+    lists.setAttribute("style", "display:block;")
+    }
+    if (menu.innerHTML == 'menu▽') {
+      menu.innerHTML = 'close△';
+    } else{
+      menu.innerHTML = 'menu▽';
+    }
+  })
+}
+
+window.addEventListener('load', pullDown)
